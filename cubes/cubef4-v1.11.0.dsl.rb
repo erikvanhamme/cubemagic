1ea12e7d5fedab7ebeec746ee401fb7847ddbc36
Erik Van Hamme <erik.vanhamme@gmail.com>
2016-05-15 01:32:04 +0200

    Added the licensing information.

--- cubes/cubef4-v1.11.0.dsl.rb
+++ cubes/cubef4-v1.11.0.dsl.rb
@@ -1,3 +1,17 @@
+# Copyright 2016 Erik Van Hamme
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Descriptor file for CubeF4 V1.11.0.
 cube 'f4', 'v1.11.0', 'STM32Cube_FW_F4_V1.11.0' do
 
