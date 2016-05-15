972a3f9e28cb75ecef5f87aa28bd914a2c3cf0c3
Erik Van Hamme <erik.vanhamme@gmail.com>
2016-05-16 00:08:32 +0200

    Added command line option parsing.

--- cubemagic.rb
+++ cubemagic.rb
@@ -21,12 +21,11 @@ $:.unshift File.expand_path('./support', $cubemagic_dir)
 require 'dsl.rb'
 require 'project.rb'
 
+require 'optparse'
 require 'readline'
 require 'zip'
 
 # -- Main Program ------------------------------------------------------------------------------------------------------
-
-project = Project.new(ARGV[0])
 
 class ComponentUsedFilter
   def filter(component, project)
@@ -35,6 +34,15 @@ class ComponentUsedFilter
       match = false
     end
     match
+  end
+end
+
+class CmdLine
+  attr_accessor :actions, :options, :project, :template_unpack_dest
+
+  def initialize
+    @actions = {}
+    @options = {}
   end
 end
 
@@ -145,7 +153,7 @@ def unpack_headers(zipfile, folder, dest, overwrite = false)
   end
 end
 
-def unpack_cube(project, overwrite = false)
+def unpack_cube(zipfile, project, overwrite = false)
   filter = ComponentUsedFilter.new
 
   header_paths = []
@@ -159,11 +167,6 @@ def unpack_cube(project, overwrite = false)
   file_paths += merge(project.cube.components, 'sys_srcs', project, [filter])
   file_paths.sort!
 
-  zipfile_name = "#{File.dirname(__FILE__)}/cubes/cube#{project.cube.name}-#{project.cube.version}.zip"
-
-  puts "Loading cube zip: #{zipfile_name}"
-  zipfile = Zip::File.open(zipfile_name)
-
   file_paths.each do |file|
     zipname = file.sub("cube#{project.cube.name}", project.cube.root)
     unpack_file(zipfile, zipname, file, overwrite)
@@ -173,15 +176,13 @@ def unpack_cube(project, overwrite = false)
     zipname = header_path.sub("cube#{project.cube.name}", project.cube.root)
     unpack_headers(zipfile, zipname, header_path, overwrite)
   end
-
-  zipfile.close
 end
 
 def template_prompt(prompt)
   Readline.readline(prompt, true).squeeze(' ').strip.downcase
 end
 
-def unpack_templates(project, destdir, prompt, overwrite = false)
+def unpack_templates(zipfile, project, destdir, prompt, overwrite = false)
   if prompt
     overwrite = true
   end
@@ -189,13 +190,8 @@ def unpack_templates(project, destdir, prompt, overwrite = false)
   filter = ComponentUsedFilter.new
 
   templates = []
-  templates += merge(project.cube.components, 'templates', project, [filter]);
+  templates += merge(project.cube.components, 'templates', project, [filter])
   # templates.sort! # TODO: add sorter.
-
-  zipfile_name = "#{File.dirname(__FILE__)}/cubes/cube#{project.cube.name}-#{project.cube.version}.zip"
-
-  puts "Loading cube zip: #{zipfile_name}"
-  zipfile = Zip::File.open(zipfile_name)
 
   templates.each do |template|
     zipname = template.template.sub("cube#{project.cube.name}", project.cube.root)
@@ -207,7 +203,7 @@ def unpack_templates(project, destdir, prompt, overwrite = false)
 
     if prompt
       response = template_prompt("\nUnpack template: #{template.template}\nYes or no? [no] : ")
-      if (response == 'yes')
+      if response == 'yes'
         unpack_file(zipfile, zipname, destname, overwrite)
       end
     else
@@ -216,12 +212,122 @@ def unpack_templates(project, destdir, prompt, overwrite = false)
       end
     end
   end
+end
 
+# Variables.
+cmd_line = CmdLine.new
+project = nil
+zipfile = nil
+
+# Parse command line options.
+OptionParser.new do |opts|
+  opts.banner = 'Usage: cubemagic.rb [options]'
+
+  opts.on('-u', '--unpack-cube', 'Unpacks the selected cube to the cubef? directory.') do |u|
+    cmd_line.actions[:unpack_cube] = u
+  end
+
+  opts.on('-t', '--unpack-templates DESTINATION', 'Unpacks the templates in the cube to the specified directory.') do |ut|
+    cmd_line.actions[:unpack_templates] = true
+    cmd_line.template_unpack_dest = ut
+
+  end
+
+  opts.on('-p', '--prompt', 'Ask user input.') do |p|
+    cmd_line.options[:prompt] = p
+  end
+
+  opts.on('-o', '--overwrite', 'Overwrites existing files.') do |o|
+    cmd_line.options[:overwrite] = o
+  end
+
+  opts.on('-m', '--makefile', 'Generates the Makefile for the project.') do |m|
+    cmd_line.actions[:makefile] = m
+  end
+
+  opts.on('-l', '--linker-script', 'Generates the linker script for the project.') do |l|
+    cmd_line.actions[:linker_script] = l
+  end
+
+  opts.on('-q', '--qtcreator-project', 'Generates the Qt creator project files for the project.') do |q|
+    cmd_line.actions[:qtcreator_project] = q
+  end
+
+  opts.on('-f', '--project-file PROJECT_FILE', 'Specifies the project file.') do |f|
+    cmd_line.project = f
+  end
+
+  opts.on('-h', '--help', 'Displays this help text.') do
+    puts opts
+    exit 0
+  end
+end.parse!
+
+# Further expand on the actions.
+if cmd_line.actions.length > 0
+  cmd_line.actions[:load_project] = true
+end
+
+if cmd_line.actions.has_key?(:unpack_cube) || cmd_line.actions.has_key?(:unpack_templates)
+  cmd_line.actions[:open_zip] = true
+  cmd_line.actions[:close_zip] = true
+end
+
+# Fill in the unspecified options.
+unless cmd_line.options[:prompt]
+  cmd_line.options[:prompt] = false
+end
+unless cmd_line.options[:overwrite]
+  cmd_line.options[:overwrite] = false
+end
+
+# Load the project if available and needed.
+if cmd_line.actions[:load_project]
+  if cmd_line.project
+    puts "Loading project: #{cmd_line.project}"
+    project = Project.new(cmd_line.project)
+  else
+    puts 'WARNING: no project file specified.'
+    exit 1
+  end
+end
+
+# Open zip file if needed.
+if cmd_line.actions[:open_zip]
+  zipfile_name = "#{File.dirname(__FILE__)}/cubes/cube#{project.cube.name}-#{project.cube.version}.zip"
+
+  puts "Loading cube zip: #{zipfile_name}"
+  zipfile = Zip::File.open(zipfile_name)
+end
+
+# Unpack the cube if needed.
+if cmd_line.actions[:unpack_cube]
+  unpack_cube(zipfile, project, cmd_line.options[:overwrite])
+end
+
+# Unpack the templates if needed.
+if cmd_line.actions[:unpack_templates]
+  unpack_templates(zipfile, project, cmd_line.template_unpack_dest, cmd_line.options[:prompt], cmd_line.options[:overwrite])
+end
+
+# Close zip file if needed.
+if cmd_line.actions[:close_zip]
   zipfile.close
 end
 
-# unpack_cube(project, true)
+# Generate the Makefile if needed.
+if cmd_line.actions[:makefile]
+  # TODO: implement me
+end
 
-unpack_templates(project, './conf', true)
+# Generate the linker script if needed.
+if cmd_line.actions[:linker_script]
+  # TODO: implement me
+end
+
+# Generate the QT Creator project files if needed.
+if cmd_line.actions[:qtcreator_project]
+  # TODO: implement me
+end
 
 puts 'Done...'
